14e118d36c35d19337777b7308197fc33be82c56
Razonek <raging@onet.eu>
2017-02-04 19:29:36 +0100

    Parsing info from server

---
+++ Hurricane/Parser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Hurricane
+{
+    public static class Parser
+    {
+
+        private static string regexForOnlineCount = @"\b\d*";
+        private static string regexForDetectionChance = @"[a-z]{8,10}";
+        private static Tuple<int, string> requestOutput;
+
+        public static Tuple<int,string> ParseRequest(string callBack)
+        {
+
+            if (callBack != null && callBack != String.Empty)
+            {
+                Match onlineCount = Regex.Match(callBack, regexForOnlineCount);
+                Match detectionChance = Regex.Match(callBack, regexForDetectionChance);
+
+                int onlinePlayers = Int32.Parse(onlineCount.ToString());
+
+                requestOutput = new Tuple<int, string>(onlinePlayers, detectionChance.ToString().ToUpper());
+            }
+            else
+                requestOutput = new Tuple<int, string>(0, "Connection error");           
+
+            return requestOutput;
+        }
+    }
+}
