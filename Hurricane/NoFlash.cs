5e0b6c42ec67a4cba424704058909713b504ca74
Razonek <raging@onet.eu>
2017-02-05 13:07:28 +0100

    No flash function

---
+++ Hurricane/NoFlash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hurricane
+{
+    public class NoFlash : Player
+    {
+
+        public void SkipFlashVisible()
+        {
+            if (Game.Read(Player.GetValue() + Offsets.m_flFlashMaxAlpha) > 0)
+                Game.WriteFloat(0.0f, Player.GetValue() + Offsets.m_flFlashMaxAlpha);
+        }
+
+
+
+
+
+    }
+}
